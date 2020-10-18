8d707e57b4069a20693c57e08432cb4eab66d5ce
Kestutis Arlauskas <kestutisa@gmail.com>
2020-10-18 16:53:07 +0300

    first test

---
+++ tests/InfoTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class InfoTest extends TestCase
+{
+    public function testFirst()
+    {
+        $this->assertTrue(true);
+    }
+}
