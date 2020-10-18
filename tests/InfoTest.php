78b3969ab0f471afbc08db8f3b0dbc1510c510b8
Kestutis Arlauskas <kestutisa@gmail.com>
2020-10-18 17:00:38 +0300

    info class

--- tests/InfoTest.php
+++ tests/InfoTest.php
@@ -2,12 +2,14 @@
 
 namespace App\Tests;
 
+use App\Info\Info;
 use PHPUnit\Framework\TestCase;
 
 class InfoTest extends TestCase
 {
     public function testFirst()
     {
+        $info = new Info();
         $this->assertTrue(true);
     }
 }
